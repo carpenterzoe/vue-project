910c109becf2eb75b6a25c444c2b5449f0f63251
Zoe <ranxia20@163.com>
2019-03-31 11:17:19 +0800

    init vueproject

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './lib/mui/css/mui.min.css'
+import { Header } from 'mint-ui'
+import 'mint-ui/lib/style.min.css'
+
 Vue.config.productionTip = false
+Vue.component(Header.name, Header) 
 
 new Vue({
   router,
