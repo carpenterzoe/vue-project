b1f41ca4c27c0d14f2e7020dcdeaca5d30e59ce5
Zoe <ranxia20@163.com>
2019-04-16 00:58:07 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,11 @@ module.exports = {
   assetsDir: "./",
   devServer: {
     proxy: {
-      'api': {
+      '/api': {
         target: 'http://www.liulongbin.top:3005/',
-        ws: true,
         changeOrigin: true,
         pathRewrite: {
-          '^api': 'api'
+          '^/api': '/api'
         }
       },
     }
